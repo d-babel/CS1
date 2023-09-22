9f42542eac66d4f7d68c9752f8c6506cf7c7039d
d-babel <143659056+d-babel@users.noreply.github.com>
2023-09-22 20:39:22 +0000

    Fri, Sep 22, 2023, 1:39 PM -07:00

--- 2324/chapter1/examples/dog_years.c
+++ 2324/chapter1/examples/dog_years.c
@@ -1,26 +1,15 @@
 #include <cs50.dev>
 #include <stdio.h>
 
-// Function to convert dog years to human years
-double dogToHumanYears(double dogAge) {
-    return dogAge * 7;
-}
-
 int main() {
-    // Ask the user for their dog's age as a double
-    double dogAge;
-    printf("Please enter your dog's age in dog years: ");
-    scanf("%lf", &dogAge);
+    int userAge;
+    printf("Please enter your age: ");
+    scanf("%d", &userAge);
 
-    // Calculate the dog's age in human years
-    double humanAge = dogToHumanYears(dogAge);
+    // Assuming 1 human year is equivalent to 7 dog years
+    float dogYears = userAge * 7.0;
 
-    // Format the result to remove trailing zeros
-    char formattedAge[20];
-    snprintf(formattedAge, sizeof(formattedAge), "%.2lf", humanAge);
-
-    // Print the result
-    printf("Your dog is approximately %s years old in human years.\n", formattedAge);
+    printf("You are %.2f years old in dog years.\n", dogYears);
 
     return 0;
 }
