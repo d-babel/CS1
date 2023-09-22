0fd6cfd008ffe84337909bdd8d850e0b73937f5c
d-babel <143659056+d-babel@users.noreply.github.com>
2023-09-22 20:19:17 +0000

    Fri, Sep 22, 2023, 1:19 PM -07:00

---
+++ 2324/chapter1/examples/dog_years.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <ctdio.h>
+
+int main(void)
+{
+    //ask the user for their dogs age
+
+
+    //figure out how odl the dog is in human years
+
+    
+    //print result
+
+
+
+}
