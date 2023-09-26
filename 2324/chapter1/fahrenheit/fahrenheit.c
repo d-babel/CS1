25fe758b6a78dd6e3c1179c329a161a0e3d2381f
d-babel <143659056+d-babel@users.noreply.github.com>
2023-09-26 19:46:51 +0000

    Tue, Sep 26, 2023, 12:46 PM -07:00

--- 2324/chapter1/fahrenheit/fahrenheit.c
+++ 2324/chapter1/fahrenheit/fahrenheit.c
@@ -6,7 +6,6 @@ int main(void)
     float celsius = get_float("C: ");
 
     float fahrenheit = (celsius * 9 / 5) + 32;
-    
-    printf("F: %.1f\n", fahrenheit);
 
+    printf("F: %.1f\n", fahrenheit);
 }
