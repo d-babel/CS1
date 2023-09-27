69fa9789f34b01dd319dee841a545b97580e7766
d-babel <143659056+d-babel@users.noreply.github.com>
2023-09-27 18:52:53 +0000

    Wed, Sep 27, 2023, 11:52 AM -07:00

--- 2324/chapter1/scrabble/scrabble.c
+++ 2324/chapter1/scrabble/scrabble.c
@@ -1,37 +1,46 @@
-#include <cs50.h>
-#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-// Point values for each letter (A to Z)
-int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+int LETTER_SCORES[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
-// Function to calculate the score of a word
-int compute_score(string word)
-{
+int calculate_score(char* word) {
     int score = 0;
+    int length = strlen(word);
 
-    for (int i = 0, n = strlen(word); i < n; i++)
-    {
-        char c = toupper(word[i]);
-
-        // Calculate score
-        score += isalpha(c) ? POINTS[c - 'A'] : 0;
+    for (int i = 0; i < length; i++) {
+        char letter = word[i];
+        if (isalpha(letter)) {
+            int index = toupper(letter) - 'A';
+            score += LETTER_SCORES[index];
+        }
     }
 
     return score;
 }
 
-int main(void)
-{
-    // Get input from players
-    string w1 = get_string("Player 1: "), w2 = get_string("Player 2: ");
+int main() {
+    char* word1 = (char*)malloc(100 * sizeof(char));
+    char* word2 = (char*)malloc(100 * sizeof(char));
 
-    // Calculate scores for each player
-    int s1 = compute_score(w1), s2 = compute_score(w2);
+    printf("Enter word 1: ");
+    scanf("%s", word1);
+    printf("Enter word 2: ");
+    scanf("%s", word2);
 
-    // Determine the winner or tie
-    printf(s1 > s2 ? "Player 1 wins!\n" : s2 > s1 ? "Player 2 wins!\n" : strcmp(w1, w2) == 0 ? "Tie!\n" : "Tie!\n");
+    int score1 = calculate_score(word1);
+    int score2 = calculate_score(word2);
+
+    if (score1 > score2) {
+        printf("Player 1 wins!\n");
+    } else if (score2 > score1) {
+        printf("Player 2 wins!\n");
+    } else {
+        printf("Tie!\n");
+    }
+
+    free(word1);
+    free(word2);
 
     return 0;
 }
