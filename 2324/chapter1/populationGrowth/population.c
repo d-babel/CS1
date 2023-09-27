22190ccbbb5f8bc8c7a28514388a731bcd958903
d-babel <143659056+d-babel@users.noreply.github.com>
2023-09-27 18:51:39 +0000

    Wed, Sep 27, 2023, 11:51 AM -07:00

--- 2324/chapter1/populationGrowth/population.c
+++ 2324/chapter1/populationGrowth/population.c
@@ -1,35 +1,29 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)
-{
-    // Ask for starting population size
-    int start_size;
-    do
-    {
-        start_size = get_int("Start size: ");
-    }
-    while (start_size < 9);
+int main(void) {
+    // Request the initial population size
+    int start;
+    do {
+        start = get_int("Enter the starting population: ");
+    } while (start < 9);
 
-    // Ask for ending population size
-    int end_size;
-    do
-    {
-        end_size = get_int("End size: ");
-    }
-    while (end_size < start_size);
+    // Request the target population size
+    int end;
+    do {
+        end = get_int("Enter the ending population: ");
+    } while (end < start);
 
     int years = 0;
 
-    // Calculate number of years required for the population to reach end size
-    while (start_size < end_size)
-    {
-        int born = start_size / 3;
-        int passed_away = start_size / 4;
-        start_size = start_size + born - passed_away;
+    // Calculate the number of years needed to reach the target population
+    while (start < end) {
+        int births = start / 3;
+        int deaths = start / 4;
+        start = start + births - deaths;
         years++;
     }
 
-    // Print number of years required
+    // Output the number of years needed
     printf("Years: %i\n", years);
 }
