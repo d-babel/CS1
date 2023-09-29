1e494e9317240c0259330822dbbcff595598f355
d-babel <143659056+d-babel@users.noreply.github.com>
2023-09-29 16:49:46 +0000

    Fri, Sep 29, 2023, 9:49 AM -07:00

--- 2324/chapter1/examples/positive/positive.c
+++ 2324/chapter1/examples/positive/positive.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 
 int main(void) {
+    int pos;
 
-    int pos = get_int("Enter a positive number:");
+    pos = get_int("Enter a positive number: ");
 
     while (pos < 0) {
-
+        printf("Please enter a positive number: ");
+        pos = get_int();
     }
-    printf("The positive number is %i \n", pos)
 
-
+    printf("The positive number is %i\n", pos);
 }
