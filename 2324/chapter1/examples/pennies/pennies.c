bf3c8f48647261f64557b863fa334883edf46ce0
d-babel <143659056+d-babel@users.noreply.github.com>
2023-10-02 21:47:07 +0000

    Mon, Oct 2, 2023, 2:47 PM -07:00

--- 2324/chapter1/examples/pennies/pennies.c
+++ 2324/chapter1/examples/pennies/pennies.c
@@ -4,7 +4,7 @@
 
 int main(void)
 {
-    int daysOfMonth, day;                       // num of days in month and current day
+    int daysOfMonth, day; // num of days in month and current day
     long long initialPennies, totalPennies = 0; // pennies on the first day and total pennies
 
     do
@@ -15,9 +15,9 @@ int main(void)
 
     do
     {
-        initialPennies = get_long_long("Enter initial pennies: "); // starting pennies
+        initialPennies = get_long_long("Enter initial pennies: "); // starting pennies (positive)
     }
-    while (initialPennies < 1); // ensure initial pennies are greater than or equal to 1
+    while (initialPennies <= 0);
 
     for (day = 0; day < daysOfMonth; day++) // loop through days
     {
@@ -25,13 +25,5 @@ int main(void)
     }
 
     double totalDollars = totalPennies / 100.0; // convert pennies to dollars
-
-    if (totalDollars >= 0)
-    {
-        printf("Total amount: $%.2f\n", totalDollars); // print total amount
-    }
-    else
-    {
-        printf("You are %.2f IN DEBT\n", -totalDollars); // print debt message
-    }
+    printf("Total amount: $%.2f\n", totalDollars); // print total amount
 }
