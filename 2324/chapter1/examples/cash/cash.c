b83fa42f72b5c73a3c777905e3376bed65330816
d-babel <143659056+d-babel@users.noreply.github.com>
2023-10-04 20:42:36 +0000

    Wed, Oct 4, 2023, 1:42 PM -07:00

--- 2324/chapter1/examples/cash/cash.c
+++ 2324/chapter1/examples/cash/cash.c
@@ -1,12 +1,17 @@
-// Cash by Drew Babel
 #include <stdio.h>
 #include <cs50.h>
 #include <math.h>
 
+// Define constants for coin values
+const int QUARTER = 25;
+const int DIME = 10;
+const int NICKEL = 5;
+const int PENNY = 1;
+
 int main(void)
 {
     float change_owed;
-    int coins[] = {25, 10, 5, 1};  // Array to store coin values
+    int coins[] = {QUARTER, DIME, NICKEL, PENNY};  // Array to store coin values
     int total_coins = 0;  // Variable to store the total number of coins
 
     do
@@ -17,7 +22,7 @@ int main(void)
 
     int cents = round(change_owed * 100);  // Convert dollars to cents and round
 
-    // Calculate the minimum number of coins required
+    // Calculate minimum number of coins required
     for (int i = cents; i > 0;)  // Iterate while there is change left
     {
         for (int j = 0; i >= coins[j]; i -= coins[j++], total_coins++);  // Calculate each coin
