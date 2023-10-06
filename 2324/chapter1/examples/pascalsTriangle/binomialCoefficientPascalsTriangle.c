075a6a0d44fd5b0c288c84b33535dba4c73eb846
d-babel <143659056+d-babel@users.noreply.github.com>
2023-10-06 18:44:50 +0000

    Fri, Oct 6, 2023, 11:44 AM -07:00

--- 2324/chapter1/examples/pascalsTriangle/binomialCoefficientPascalsTriangle.c
+++ 2324/chapter1/examples/pascalsTriangle/binomialCoefficientPascalsTriangle.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 
-// calculate the factorial of a number
+// calculate the factorial of a number (unsigned used to prevent negatives)
 unsigned long long factorial(int n) {
     unsigned long long fact = 1;
     for (int i = 2; i <= n; i++) fact *= i; // multiply by all integers from 2 to n
