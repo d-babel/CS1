56c1b702c354dea434e3a437692386b0826ebcc2
d-babel <143659056+d-babel@users.noreply.github.com>
2023-10-06 18:10:53 +0000

    Fri, Oct 6, 2023, 11:10 AM -07:00

--- 2324/chapter1/examples/cash/cash.c
+++ 2324/chapter1/examples/cash/cash.c
@@ -1,4 +1,4 @@
-//Cash by Drew Babel
+// Cash by Drew Babel
 
 #include <stdio.h>
 #include <cs50.h>
@@ -6,12 +6,16 @@
 
 int main(void) {
     const int COINS[] = {25, 10, 5, 1};
+
+    // keep track of the number of coins used and the change owed in cents.
     int coins = 0, cents = round(get_float("Change owed: ") * 100);
 
+    // Ensure that the user inputs a non-negative value for change owed.
     while (cents < 0) {
         cents = round(get_float("Change owed: ") * 100);
     }
 
+    // Iterate through the array of coin values and calculate the number of each coin needed.
     for (int i = 0; i < 4; i++) {
         while (cents >= COINS[i]) {
             cents -= COINS[i];
@@ -19,5 +23,6 @@ int main(void) {
         }
     }
 
+    // Print the total number of coins used to make the change.
     printf("%d\n", coins);
 }
