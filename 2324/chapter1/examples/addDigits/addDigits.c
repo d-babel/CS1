f49d372e0c6b8741f569f27960e62ee5c64317ee
d-babel <143659056+d-babel@users.noreply.github.com>
2023-10-11 21:44:23 +0000

    Wed, Oct 11, 2023, 2:44 PM -07:00

--- 2324/chapter1/examples/addDigits/addDigits.c
+++ 2324/chapter1/examples/addDigits/addDigits.c
@@ -1,23 +1,34 @@
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 
-int main() {
+int main()
+{
     char input[100];
     int sum = 0;
     int validInput = 0;
 
-    while (!validInput) {
+    while (!validInput)
+    {
         printf("Enter a number: ");
-        if (fgets(input, sizeof(input), stdin) == NULL) {
+        if (fgets(input, sizeof(input), stdin) == NULL)
+        {
+            // check if fgets encountered an error (e.g., end of file or read error).
+            // if so handle the error as needed
         }
 
-        validInput = 1;
-        sum = 0;
+        validInput = 1; // assume valid input until proven otherwise.
+        sum = 0;        // initialize the sum to zero.
 
-        for (int i = 0; input[i] != '\0'; i++) {
-            if (isdigit(input[i])) {
+        for (int i = 0; input[i] != '\0'; i++)
+        {
+            if (isdigit(input[i]))
+            {
+                // if the current character is a digit, convert it to an integer, add it
                 sum += input[i] - '0';
-            } else if (input[i] != '\n') {
+            }
+            else if (input[i] != '\n')
+            {
+                // if current character is not digit and not  newline character, input invalid
                 validInput = 0;
             }
         }
