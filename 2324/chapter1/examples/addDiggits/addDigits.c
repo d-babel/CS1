edec11305d16226d1f135c53ed06527458ce46a3
d-babel <143659056+d-babel@users.noreply.github.com>
2023-10-11 21:22:15 +0000

    Wed, Oct 11, 2023, 2:22 PM -07:00

--- 2324/chapter1/examples/addDiggits/addDigits.c
+++ 2324/chapter1/examples/addDiggits/addDigits.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
     printf("Enter a number: ");
-    int sum = 0;
-    char c;
+    int sum = 0;  
+    char c;  // Declare a variable to store the input character
 
-    while ((c = getchar()) != '\n')
-    { // check for empty input
-        if (c >= '0' && c <= '9')
-        {
-            // if the character is a digit, convert to integer, add to sum
+    // Read characters until Enter (newline) is pressed
+    while ((c = getchar()) != '\n') {  // Check for empty input
+
+        if (c >= '0' && c <= '9') {
+            // If the character is a digit, convert to an integer, add to sum
             sum += c - '0';
         }
     }
 
-    printf("The sum of the individual digits is: %d\n", sum);
+    printf("The sum of the individual digits is: %d\n", sum);  // Print the sum
 }
