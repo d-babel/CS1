94c6e1b3acee7fb4b807fc1f92d5db402b0ebe69
d-babel <143659056+d-babel@users.noreply.github.com>
2023-10-17 19:04:30 +0000

    Tue, Oct 17, 2023, 12:04 PM -07:00

--- 2324/chapter1/examples/credit/credit.c
+++ 2324/chapter1/examples/credit/credit.c
@@ -1,65 +1,66 @@
 #include <cs50.h>
 #include <math.h>
 #include <stdio.h>
-int main(void)
-{
-    // Ask for input
-    long long cardnumber;
-    do
-    {
-        cardnumber = get_long_long("Number: ");
+
+// Define constants for clarity
+#define AMEX_PREFIX_1 37
+#define AMEX_PREFIX_2 34
+#define VISA_PREFIX 4
+#define MASTERCARD_PREFIX_1 51
+#define MASTERCARD_PREFIX_2 52
+#define MASTERCARD_PREFIX_3 53
+#define MASTERCARD_PREFIX_4 54
+#define MASTERCARD_PREFIX_5 55
+
+int main(void) {
+    // Prompt for input
+    long long card_number;
+    do {
+        card_number = get_long_long("Enter card number: ");
+    } while (card_number < 0);
+
+    // Calculate the number of digits
+    int digit_count = 0;
+    long long number = card_number;
+    while (number > 0) {
+        number = number / 10;
+        digit_count++;
     }
-    while (cardnumber < 0);
-    //Count the number of digits
-    int count = 0;
-    long long i = cardnumber;
-    while (i > 0)
-    {
-        i = i / 10;
-        count++;
-    }
-    //Multiply every other digit by 2, starting with the number’s second-to-last digit, and then add those products’ digits together. Add the sum to the sum of the digits that weren’t multiplied by 2.
-    long long j = cardnumber;
+
+    // Calculate checksum
+    long long temp = card_number;
     int sum1 = 0;
     int remainder1;
     int remainder0;
-    while (j != 0)
-    {
-        remainder1 = 2 * ((j % 100) / 10);
+    while (temp != 0) {
+        remainder1 = 2 * ((temp % 100) / 10);
         remainder0 = remainder1 % 10 + round(remainder1 / 10);
-        sum1 = sum1 + remainder0 ;
-        j = j / 100;
+        sum1 = sum1 + remainder0;
+        temp = temp / 100;
     }
-    //Add the sum to the sum of the digits that weren’t multiplied by 2.
-    long long k = cardnumber;
+
+    // Add the sum to the digits that weren't multiplied by 2
+    long long temp2 = card_number;
     int sum2 = 0;
     int remainder2;
-    while (k != 0)
-    {
-        remainder2 = k % 10;
+    while (temp2 != 0) {
+        remainder2 = temp2 % 10;
         sum2 = sum2 + remainder2;
-        k = k / 100;
+        temp2 = temp2 / 100;
     }
+
     int checksum = sum1 + sum2;
-    //If the total’s last digit is 0 (or, put more formally, if the total modulo 10 is congruent to 0), the number is valid!
-    if (checksum % 10 == 0 && count == 15 && (floor(cardnumber / 10000000000000) == 37 || floor(cardnumber / 10000000000000) == 34))
-    {
-        printf("AMEX\n");
-    }
-    else if (checksum % 10 == 0 && count == 13 && floor(cardnumber / 1000000000000) == 4)
-    {
-        printf("VISA\n");
-    }
-    else if (checksum % 10 == 0 && count == 16 && floor(cardnumber / 1000000000000000) == 4)
-    {
-        printf("VISA\n");
-    }
-    else if (checksum % 10 == 0 && count == 16 && (floor(cardnumber / 100000000000000) == 51 || floor(cardnumber / 100000000000000) == 52 || floor(cardnumber / 100000000000000) == 53 || floor(cardnumber / 100000000000000) == 54 || floor(cardnumber / 100000000000000) == 55))
-    {
-        printf("MASTERCARD\n");
-    }
-    else
-    {
-        printf("INVALID\n");
+
+    // Check the card type
+    if (checksum % 10 == 0 && digit_count == 15 && (floor(card_number / 10000000000000) == AMEX_PREFIX_1 || floor(card_number / 10000000000000) == AMEX_PREFIX_2)) {
+        printf("Card Type: AMEX\n");
+    } else if (checksum % 10 == 0 && digit_count == 13 && floor(card_number / 1000000000000) == VISA_PREFIX) {
+        printf("Card Type: VISA\n");
+    } else if (checksum % 10 == 0 && digit_count == 16 && floor(card_number / 1000000000000000) == VISA_PREFIX) {
+        printf("Card Type: VISA\n");
+    } else if (checksum % 10 == 0 && digit_count == 16 && (floor(card_number / 100000000000000) == MASTERCARD_PREFIX_1 || floor(card_number / 100000000000000) == MASTERCARD_PREFIX_2 || floor(card_number / 100000000000000) == MASTERCARD_PREFIX_3 || floor(card_number / 100000000000000) == MASTERCARD_PREFIX_4 || floor(card_number / 100000000000000) == MASTERCARD_PREFIX_5)) {
+        printf("Card Type: MASTERCARD\n");
+    } else {
+        printf("Card Type: INVALID\n");
     }
 }
