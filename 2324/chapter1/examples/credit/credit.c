5527742d50ccec02b8bfd97f6adeee252012ce98
d-babel <143659056+d-babel@users.noreply.github.com>
2023-10-17 18:56:29 +0000

    Tue, Oct 17, 2023, 11:56 AM -07:00

--- 2324/chapter1/examples/credit/credit.c
+++ 2324/chapter1/examples/credit/credit.c
@@ -1,40 +1,42 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <math.h>
 
-#define AMEX_LENGTH 15
-#define MASTERCARD_LENGTH 16
-#define DIVISOR 10
-#define MULTIPLIER 2
-#define SUM_CHECK 0
+#define LENGTH_AMEX 15
+#define LENGTH_VISA1 13
+#define LENGTH_VISA2 16
+#define LENGTH_MC 16
+#define PREFIX_AM 34
+#define PREFIX_MC1 51
+#define PREFIX_MC2 55
+#define PREFIX_VISA 4
+#define INVALID 0
 
-int get_length(long n);
-bool check_validity(int length, long n);
+bool check_validity(long n, int length);
+bool begins_with(long n, int length, int prefix);
 
 int main(void)
 {
-    long number;
-    do { number = get_long("Number: "); } while (number < SUM_CHECK);
+    long card_number = get_long("Number: ");
+    int length = round(log10(card_number) + 1);
+    bool valid = check_validity(card_number, length);
 
-    int length = get_length(number);
-    bool validity = check_validity(length, number);
-
-    printf(validity ? ((length == AMEX_LENGTH) ? "AMEX\n" : (length == MASTERCARD_LENGTH) ? "MASTERCARD\n" : "VISA\n") : "INVALID\n");
+    if (valid && length == LENGTH_AMEX && begins_with(card_number, length, PREFIX_AM)) printf("AMEX\n");
+    else if (valid && (length == LENGTH_VISA1 || length == LENGTH_VISA2) && begins_with(card_number, length, PREFIX_VISA)) printf("VISA\n");
+    else if (valid && length == LENGTH_MC && begins_with(card_number, length, PREFIX_MC1) && begins_with(card_number, length, PREFIX_MC2)) printf("MASTERCARD\n");
+    else printf("INVALID\n");
 }
 
-int get_length(long n)
+bool check_validity(long n, int length)
 {
-    int length = SUM_CHECK;
-    for (; n != SUM_CHECK; n /= DIVISOR, length++);
-    return length;
+    int sum = INVALID;
+    for (int i = 0; i < length; i++, n /= 10)
+        sum += i % 2 == INVALID ? n % 10 : 2 * (n % 10) / 10 + 2 * (n % 10) % 10;
+
+    return sum % 10 == INVALID;
 }
 
-bool check_validity(int length, long n)
+bool begins_with(long n, int len, int prefix)
 {
-    int sum = SUM_CHECK, i = 1;
-    for (; i <= length; i++, n /= DIVISOR)
-    {
-        int digit = n % DIVISOR;
-        sum += (i % MULTIPLIER == SUM_CHECK) ? ((MULTIPLIER * digit) / DIVISOR + (MULTIPLIER * digit) % DIVISOR) : digit;
-    }
-    return sum % DIVISOR == SUM_CHECK;
+    return n / pow(10, len - (round(log10(prefix) + 1))) == prefix;
 }
