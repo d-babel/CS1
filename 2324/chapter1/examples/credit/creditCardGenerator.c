98062991ee7bf85a9373a62d64c2374f95c91390
d-babel <143659056+d-babel@users.noreply.github.com>
2023-10-20 21:09:01 +0000

    Fri, Oct 20, 2023, 2:09 PM -07:00

--- 2324/chapter1/examples/credit/creditCardGenerator.c
+++ 2324/chapter1/examples/credit/creditCardGenerator.c
@@ -166,13 +166,9 @@ int main(void) {
         int card_length = snprintf(NULL, 0, "%lld", card_number);
 
         // Print the table row with proper spacing and numbering
-            int num_digits = snprintf(NULL, 0, "%d", num_cards);
-            int card_num_width = card_number_column_width - num_digits;
-            printf("|%*d. |%-*s|%-*lld|\n", num_digits, i + 1, brand_column_width - 3, chosen_brand, card_num_width, card_number);
+           printf("|%2d. |%-*s|%-*lld|\n", i + 1, brand_column_width - 3, chosen_brand, card_number_column_width - 1, card_number);
         if (i == num_cards - 1) {
             print_horizontal_line(total_width);
         }
     }
-
-    return 0;
 }
