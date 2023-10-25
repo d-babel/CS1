c41741eb141dfc656fce8819500e2fbcdf500d85
d-babel <143659056+d-babel@users.noreply.github.com>
2023-10-25 20:24:25 +0000

    Wed, Oct 25, 2023, 1:24 PM -07:00

---
+++ 2324/chapter1/examples/mario/mario.c
@@ -0,0 +1,15 @@
+// Mario by Drew Babel
+
+#include <cs50.h>
+#include <stdio.h>
+
+
+int main(void){
+    int height = getint("Height:");
+    for(spaces = height; spaces < 0; i--){
+        printf("%i",spaces-1);
+
+    }
+
+}
+
