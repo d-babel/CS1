67e78900613d70a42a97c9357557c3879a954ecc
d-babel <143659056+d-babel@users.noreply.github.com>
2023-10-25 20:36:54 +0000

    Wed, Oct 25, 2023, 1:36 PM -07:00

--- 2324/chapter1/examples/mario/mario.c
+++ 2324/chapter1/examples/mario/mario.c
@@ -1,21 +1,24 @@
 // Mario by Drew Babel
 
-//NOTES:
-//  put into functions
+// NOTES:
+//   put into functions
 
 #include <cs50.h>
 #include <stdio.h>
 
-
-int main(void){
-    //input
+int main(void)
+{
+    // input
     int height = get_int("Height:");
 
-    //spaces
-    for(int i = height; i < 0; i--){
+    // spaces
+    for (int i = height; i < 0; i--)
+    {
         printf(" ");
-
     }
-
+    for (int i = 1; i <= height; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
 }
-
