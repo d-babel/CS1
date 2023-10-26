1639a41b6febdeb687a737f038752ae9ae997549
d-babel <143659056+d-babel@users.noreply.github.com>
2023-10-26 05:26:28 +0000

    Wed, Oct 25, 2023, 10:26 PM -07:00

--- 2324/chapter1/examples/mario/mario.c
+++ 2324/chapter1/examples/mario/mario.c
@@ -12,27 +12,26 @@ int main(void)
 
     int height;
 
-    //input and check if between 1-8 inclusive
+    // input and check if between 1-8 inclusive
     do
     {
         height = get_int("Height:");
     }
     while (height < 1 || height > 8);
-    //print pyramid
-    for (int i = 1; i < height; i++){
+    // print pyramid
+    for (int i = 1; i < height; i++)
+    {
+        // spaces
+        for (int j = height; j < height - i; j++)
+        {
+            printf(" ");
+        }
 
-    // spaces
-      for (int j = height; j < height -i; j++)
-      {
-          printf(" ");
-      }
-
-    //hashtags for l
-      for (int j = 0; j < i; j++)
-      {
-          printf("#");
-      }
-     printf("\n");
+        // hashtags for l
+        for (int j = 0; j < i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
     }
-
 }
