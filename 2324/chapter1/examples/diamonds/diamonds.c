acb528aac97fd607b4e714db07f657e478cd9c88
d-babel <143659056+d-babel@users.noreply.github.com>
2023-10-27 18:35:14 +0000

    Fri, Oct 27, 2023, 11:35 AM -07:00

--- 2324/chapter1/examples/diamonds/diamonds.c
+++ 2324/chapter1/examples/diamonds/diamonds.c
@@ -6,7 +6,17 @@
 //input
 int size = get_int("Size? ");
 
-int drawDiamonds(size){
+int drawDiamonds(int n){
     // spaces start at (1/2)(size)-1, then get -1 every time until spaces = 0, then adds every time until back to (1/2)(size)-1
+    for(int i = 1; i <= (i % 2 == 0 ? 2 * n : n - 1; i--))
+    {
+        
+    }
+}
+
+
+
+int main(void)
+{
 
 }
