1bc6662fa6b06cbabcdb3117343cb96a6b0fe8d3
d-babel <143659056+d-babel@users.noreply.github.com>
2023-10-27 18:39:05 +0000

    Fri, Oct 27, 2023, 11:39 AM -07:00

--- 2324/chapter1/examples/diamonds/diamonds.c
+++ 2324/chapter1/examples/diamonds/diamonds.c
@@ -7,10 +7,18 @@
 int size = get_int("Size? ");
 
 int drawDiamonds(int n){
+    int spaces = n / 2 - 1;
+    int stars = 1;
+    int direction = -1
+
+
     // spaces start at (1/2)(size)-1, then get -1 every time until spaces = 0, then adds every time until back to (1/2)(size)-1
     for(int i = 1; i <= (i % 2 == 0 ? 2 * n : n - 1; i--))
     {
-        
+        for(int j = 0; j < spaces; j++)
+        {
+            printf(" ");
+        }
     }
 }
 
