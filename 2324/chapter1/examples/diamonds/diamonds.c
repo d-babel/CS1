f2530570ff47c5c88b7dea175407abd5fcd8304b
d-babel <143659056+d-babel@users.noreply.github.com>
2023-10-27 19:06:56 +0000

    Fri, Oct 27, 2023, 12:06 PM -07:00

--- 2324/chapter1/examples/diamonds/diamonds.c
+++ 2324/chapter1/examples/diamonds/diamonds.c
@@ -10,7 +10,7 @@ int drawDiamonds(int n){
 
     // spaces start at (1/2)(size)-1, then get -1 every time until spaces = 0, then adds every time until back to (1/2)(size)-1
 
-    for(int i = 1; i <= (i % 2 == 0 ? 2 * n : n - 1; i--)) // if even, n*2, otherwise (when its odd), *2 -1
+    for(int i = 1; i <= (i % 2 == 0 ? 2 * n : n - 1); i++) // if even, n*2, otherwise (when its odd), *2 -1
     {
         for(int j = 0; j < n; j++)
         {
@@ -37,7 +37,7 @@ int main(void)
     int n; // var for drawDiamonds
 
     do {
-        int n = get_int("Size? "); //input
+        n = get_int("Size? "); //input
     }
     while (n < 1 );
         drawDiamonds(n);
