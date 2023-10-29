b11dc85a2b3132d93c91acf7972a2b02102a04c2
d-babel <143659056+d-babel@users.noreply.github.com>
2023-10-29 01:21:21 +0000

    Sat, Oct 28, 2023, 6:21 PM -07:00

--- 2324/chapter1/examples/diamonds/diamonds.c
+++ 2324/chapter1/examples/diamonds/diamonds.c
@@ -6,13 +6,16 @@
 #include <stdio.h>
 
 void drawDiamonds(int n){
+    int spaces = n / 2; //initial spacing pattern according to length
+    int direction = -1; // -1 for decreasing, 1 for increasing
+
     // spaces start at (1/2)(size)-1, then get -1 every time until spaces = 0, then adds every time until back to (1/2)(size)-1
 
     for(int i = 1; i <= (n % 2 == 0 ? 2 * n : 2 * n - 1); i++) // if even, n*2, otherwise (when its odd), *2 -1
     {
         for(int j = 0; j < n; j++)
         {
-            if(j < spaces|| j >= n - spaces)
+            if(j < spaces || j >= n - spaces)
             {
                 printf(" ");
             } else {
