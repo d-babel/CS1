7d82496b29f5853ccc6b0664e6f7e52e32e85320
d-babel <143659056+d-babel@users.noreply.github.com>
2023-10-29 03:20:11 +0000

    Sat, Oct 28, 2023, 8:20 PM -07:00

--- 2324/chapter1/examples/diamonds/diamonds.c
+++ 2324/chapter1/examples/diamonds/diamonds.c
@@ -44,6 +44,15 @@ void drawDiamonds(int n){
         for (int i = 1; i <= (size +1) / 2; i++)
         {
             for (int j = 0; j < size - i - 1; j++)
+            {
+                printf(" "):
+            }
+            for (int j = 0; j < 2 * i - 1; j++)
+            {
+                if (j % 2 == 0) {
+                    printf()
+                }
+            }
         }
     }
 }
