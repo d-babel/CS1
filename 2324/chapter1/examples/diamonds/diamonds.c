27e2f2205ee4dfab73fd228f26c8e2917732623c
d-babel <143659056+d-babel@users.noreply.github.com>
2023-10-29 03:13:15 +0000

    Sat, Oct 28, 2023, 8:13 PM -07:00

--- 2324/chapter1/examples/diamonds/diamonds.c
+++ 2324/chapter1/examples/diamonds/diamonds.c
@@ -6,34 +6,45 @@
 #include <stdio.h>
 
 void drawDiamonds(int n){
-    int spaces = n / 2; //initial spacing pattern according to length
-    int direction = -1; // -1 for decreasing, 1 for increasing
+    // int spaces = n / 2; //initial spacing pattern according to length
+    // int direction = -1; // -1 for decreasing, 1 for increasing
 
     // spaces start at (1/2)(size)-1, then get -1 every time until spaces = 0, then adds every time until back to (1/2)(size)-1
 
-    for(int i = 1; i <= (n % 2 == 0 ? 2 * n : 2 * n - 1); i++) // if even, n*2, otherwise (when its odd), *2 -1
+    // for(int i = 1; i <= (n % 2 == 0 ? 2 * n : 2 * n - 1); i++) // if even, n*2, otherwise (when its odd), *2 -1
+    // {
+    //     for(int j = 0; j < n; j++)
+    //     {
+    //         if(j < spaces || j >= n - spaces - 1)
+    //         {
+    //             printf(" ");
+    //         } else {
+    //             printf("*");
+    //         }
+
+    //     }
+    //     printf("\n");
+
+    //     if(spaces == 0 && direction == -1){
+    //     direction = 1;
+    //     if (n % 2 == 0)
+    //     {
+    //         i--;
+    //     }
+    //     }
+    //     spaces += direction;
+
+    // }
+
+
+    // odd
+    for (size % 2 != 0)
     {
-        for(int j = 0; j < n; j++)
+        //top half
+        for (int i = 1; i <= (size +1) / 2; i++)
         {
-            if(j < spaces || j >= n - spaces - 1)
-            {
-                printf(" ");
-            } else {
-                printf("*");
-            }
-
+            for (int j = 0; j < size - i - 1; j++)
         }
-        printf("\n");
-
-        if(spaces == 0 && direction == -1){
-        direction = 1;
-        if (n % 2 == 0)
-        {
-            i--;
-        }
-        }
-        spaces += direction;
-
     }
 }
 
