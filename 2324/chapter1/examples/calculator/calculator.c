32938d73a5668a89ddf61024e3f9d70ea849f8cd
d-babel <143659056+d-babel@users.noreply.github.com>
2023-10-31 16:32:43 +0000

    Tue, Oct 31, 2023, 9:32 AM -07:00

--- 2324/chapter1/examples/calculator/calculator.c
+++ 2324/chapter1/examples/calculator/calculator.c
@@ -5,5 +5,35 @@
 
 int main(void)
 {
-    
+    //ask user for two numbers
+    int num1 = get_int("Enter a number:");
+    int num2 = get_int("Enter another number:");
+
+    // compute the sum
+    int sum = num1 + num2;
+
+    // print the sum
+    printf("The sum is %i\n", sum);
+
+}
+
+
+int addition(num1, num2);
+{
+    return num1 + num2;
+}
+
+int subtraction(num1, num2);
+{
+    return num1 + num2;
+}
+
+int multiplication(num1, num2);
+{
+    return num1 + num2;
+}
+
+int division(num1, num2);
+{
+    return num1 + num2;
 }
