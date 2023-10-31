959ecbec1c0bd9b3eb6591d2e0ddf1e48496079f
d-babel <143659056+d-babel@users.noreply.github.com>
2023-10-31 16:33:39 +0000

    Tue, Oct 31, 2023, 9:33 AM -07:00

--- 2324/chapter1/examples/calculator/calculator.c
+++ 2324/chapter1/examples/calculator/calculator.c
@@ -10,7 +10,7 @@ int main(void)
     int num2 = get_int("Enter another number:");
 
     // compute the sum
-    int sum = num1 + num2;
+    int sum = addition(num1, num2);
 
     // print the sum
     printf("The sum is %i\n", sum);
@@ -25,15 +25,15 @@ int addition(num1, num2);
 
 int subtraction(num1, num2);
 {
-    return num1 + num2;
+    return num1 - num2;
 }
 
 int multiplication(num1, num2);
 {
-    return num1 + num2;
+    return num1 * num2;
 }
 
 int division(num1, num2);
 {
-    return num1 + num2;
+    return num1 / num2;
 }
