4709b7489c2eda543ecfffc5ef2cee505a738a79
d-babel <143659056+d-babel@users.noreply.github.com>
2023-11-08 04:38:27 +0000

    Tue, Nov 7, 2023, 8:38 PM -08:00

---
+++ 2324/chapter2/examples/calc/calc copy.c
@@ -0,0 +1,102 @@
+// Calc by Drew Babel
+
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//mod opperator --> Usage: ./calc [num1] [operator] [num2]
+//index:                      0      1        2       3
+
+double mod(double num1, double num2);
+
+
+int main(int argc, string argv[])
+{
+    //check
+    if (argc != 4)
+    {
+        printf("Usage: ./calc [num1] [operator] [num2]");
+        return 1;
+    }
+
+    double num1 = atof(argv[1]);
+    double num2 = atof(argv[3]);
+    char operator = argv[2][0]; //assign the first character of the third  command line argument (index: 2) as the operator --> needed as you cant firectly assign string to char
+
+
+    // if(argv[3] = '*') //multiplication
+    // {
+    //    printf(argv[2]+argv[3]);
+    // }
+    // else if(argv[3] = '/') //division
+    // {
+
+    // }
+    // else if(argv[3] = '+') //addition
+    // {
+
+    // }
+    // else if(argv[3] = '-') //subtraction
+    // {
+
+    // }
+    // else // modulus
+    // mod();
+
+    // }
+
+    switch (operator) //better version of if else statments above
+    {
+        case 'x': //multiplication
+            printf("%f\n", num1 * num2);
+            break;
+        case '/': //division
+            if (num2 ==0)
+            {
+                printf("Cannot divide by zero\n"); //divide by zero check
+            }
+            else
+            {
+                printf("%f\n", num1 / num2);
+            }
+            break;
+        case '+': //addition
+            printf("%f\n", num1 + num2);
+            break;
+        case '-': //subtraction
+            printf("%f\n", num1 - num2);
+            break;
+        case '%': //modulus
+        //modulus operator in C only works with integers, this function handles it as a float
+            printf("%f\n", mod(num1, num2));
+            break;
+        default: //invalid input for operator
+            printf("Invalid operator '%c' ./n", operator);
+            return 1;
+    }
+}
+
+double mod(double num1, double num2)
+{
+    if (num2 == 0){
+        printf("Error: Division by zero in modulus operator not possible");
+        return 0;
+    }
+    else
+    {
+        //compute modulus for floating point numbers
+        int quotient = (int)(num1 / num2);
+        double mod = num1 - (num2 * quotient);
+        return mod;
+    }
+
+    //ex:
+    // q = (int) (10.7 / 3.28);
+    // q = (int) 3.262195;
+    // q = 3
+    // mod = 10.7 - (3.28 * 3);
+    // mod = 10.7 - 9.84
+    // mod = 0.86
+
+}
