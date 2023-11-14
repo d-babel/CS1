bcd2171bd651620d82e6a0f84408108f08bd23cc
d-babel <143659056+d-babel@users.noreply.github.com>
2023-11-14 00:21:08 +0000

    Mon, Nov 13, 2023, 4:21 PM -08:00

--- 2324/chapter2/examples/readability/readability.c
+++ 2324/chapter2/examples/readability/readability.c
@@ -20,8 +20,83 @@
 
 int main(void)
 {
+    //user input excerpt
     string text = get_string("Text: ");
+
+    //count letters, words, and sentences
+    int letters = count.letters(text);
+    int words = count_words(text);
+    int sentences = count_sentences(text);
+
+    // L is the average number of letters per 100 words
+    float L = (float)letters / (float)words * 100;
+
+    // S is the average number of sentences per 100 words
+    float S = (float)sentences / (float)words * 100;
+
+    // calculate readability index using Coleman-Liau formula
+    float index = COEFFICIENT_LETTERS_PER_100_WORDS * L - COEFFICIENT_SENTENCES_PER_100_WORDS * S - COEFFICIENT_BASE_OFFSET;
+
+    // round index to nearest whole num and find grade
+    int grade = round(index);
+    if (grade >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else if (grade < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else
+    {
+        printf("grade %i\n", grade);
+    }
+
 }
+
+//function to count letters in text
+int count_letters(string text)
+{
+    int count = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (isalpha(test[i]))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+// function to count words in text
+int count_words(strings text)
+{
+    int count = 1; // start with 1 for the last word
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (text[i] == ' ');
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+//function to count sentences in text
+int count_sentences(string text)
+{
+    int count = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
 
 // user inputs a excerpt from a book
 // DATATYPE text = get_DATATYPE("Text: ");
