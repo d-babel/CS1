388e90f6e686efca457c2d89006a7f1a8f120daa
d-babel <143659056+d-babel@users.noreply.github.com>
2023-11-15 04:32:29 +0000

    Tue, Nov 14, 2023, 8:32 PM -08:00

--- 2324/chapter2/examples/readability/readability.c
+++ 2324/chapter2/examples/readability/readability.c
@@ -1,10 +1,10 @@
 // Readability by Drew Babel
 
 #include <cs50.h>
-#include <stdio.h>
-#include <math.h>
-#include <string.h>
 #include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
 
 //#define formula? --> ask in office hours
 // define constants for Coleman-Liau index formula
@@ -17,22 +17,21 @@ int count_letters(string text);
 int count_words(string text);
 int count_sentences(string text);
 
-
 int main(void)
 {
-    //user input excerpt
+    // user input excerpt
     string text = get_string("Text: ");
 
-    //count letters, words, and sentences
+    // count letters, words, and sentences
     int letters = count_letters(text);
     int words = count_words(text);
     int sentences = count_sentences(text);
 
     // L is the average number of letters per 100 words
-    float L = (float)letters / (float)words * 100;
+    float L = (float) letters / (float) words * 100;
 
     // S is the average number of sentences per 100 words
-    float S = (float)sentences / (float)words * 100;
+    float S = (float) sentences / (float) words * 100;
 
     // calculate readability index using Coleman-Liau formula
     float index = COEFFICIENT_LETTERS_PER_100_WORDS * L - COEFFICIENT_SENTENCES_PER_100_WORDS * S - COEFFICIENT_BASE_OFFSET;
@@ -51,10 +50,9 @@ int main(void)
     {
         printf("Grade %i\n", grade);
     }
-
 }
 
-//function to count letters in text
+// function to count letters in text
 int count_letters(string text)
 {
     int count = 0;
@@ -82,7 +80,7 @@ int count_words(string text)
     return count;
 }
 
-//function to count sentences in text
+// function to count sentences in text
 int count_sentences(string text)
 {
     int count = 0;
@@ -96,14 +94,12 @@ int count_sentences(string text)
     return count;
 }
 
-
-
 // user inputs a excerpt from a book
 // DATATYPE text = get_DATATYPE("Text: ");
 
 // L = SOLVE FOR L
 // S = SOLVE FOR S
-//index = 0.0588 * L - 0.296 * S - 15.8
+// index = 0.0588 * L - 0.296 * S - 15.8
 
 // L is the average number of letters per 100 words in the text
 // S is the average number of sentences per 100 words in text
@@ -113,8 +109,6 @@ int count_sentences(string text)
 // print grade (which is rounded index)
 // if rounded index > 16, print "Grade 16+"
 // if rounded index < 1 print "Before Grade 1"
-
-
 
 // ex:
 // $ ./readability
