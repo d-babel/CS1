d59d0f5a4a3c1c40f6a78eb3e05e8dc662c6659f
d-babel <143659056+d-babel@users.noreply.github.com>
2023-11-15 21:18:34 +0000

    Wed, Nov 15, 2023, 1:18 PM -08:00

--- 2324/chapter2/examples/emoji/emoji.c
+++ 2324/chapter2/examples/emoji/emoji.c
@@ -1,10 +1,12 @@
-#include <locale.h>
-#include <stdio.h>
-#include <wchar.h>
+// #include <locale.h>
+// #include <stdio.h>
+// #include <wchar.h>
 
-int main(void)
-{
-    setlocale(LC_ALL, "");
-    wchar_t pumpkin = 0x1F383;
-    printf("%lc\n", pumpkin);
-}
+// int main(void)
+// {
+//     setlocale(LC_ALL, "");
+//     wchar_t pumpkin = 0x1F383;
+//     printf("%lc\n", pumpkin);
+// }
+
+
