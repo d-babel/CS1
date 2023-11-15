2eeae1c91fc1220d7983101ea3d6fed521d31cfd
d-babel <143659056+d-babel@users.noreply.github.com>
2023-11-15 21:22:41 +0000

    Wed, Nov 15, 2023, 1:22 PM -08:00

--- 2324/chapter2/examples/emoji/emoji.c
+++ 2324/chapter2/examples/emoji/emoji.c
@@ -9,4 +9,20 @@
 //     printf("%lc\n", pumpkin);
 // }
 
+// Emoji by Drew Babel
+
+#include <cs50.h>
+#include <ctype.h>
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wchar.h>
+
+//input
+
+// emoji function
+    // check if input starts with "U+"
+    // check if the rest of the input is hexadecimal
+    // convert and return if valid hexadecimal
 
