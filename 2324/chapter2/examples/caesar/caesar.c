a00bdac4ed9658ea511d6bd2c4460b01442a6268
d-babel <143659056+d-babel@users.noreply.github.com>
2023-11-30 03:04:05 +0000

    Wed, Nov 29, 2023, 7:04 PM -08:00

---
+++ 2324/chapter2/examples/caesar/caesar.c
@@ -0,0 +1,70 @@
+// Caesar by Drew Babel
+
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+bool is_numerical(string key);
+void encrypt(string plaintext, int key);
+
+int main(int argc, string argv[])
+{
+    // Check if the user provided the correct number of arguments
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+
+    // Check for proper key values
+    if (!is_numerical(argv[1]))
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+
+
+    // Convert the key to an integer
+    int key = atoi(argv[1]);
+
+    // Get the plaintext from the user
+    string plaintext = get_string("plaintext:  ");
+
+    // Perform Caesar cipher encryption
+    encrypt(plaintext, key);
+
+    // Print the ciphertext
+    printf("ciphertext: %s\n", plaintext);
+
+    return 0;
+}
+
+
+bool is_numerical(string key)
+{
+    for (int i = 0, n = strlen(key); i < n; i++)
+    {
+        if (!isdigit(key[i]))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+// Function to perform Caesar cipher encryption on all letters.
+// Each letter should be offset by key, wrapping around from
+// Z to A and from z to a.
+// Example:
+// caesar_encrypt("ABC xyz!", 2) changes the string to "CDE zab!"
+void encrypt(string plaintext, int key)
+{
+    // TODO — Your code here!
+
+    //use taken in encrypt to offset chars
+    //if offset value % [highest ascii value] > 0, set new char value to that
+    //loop thru word strlen times with input[i]
+}
