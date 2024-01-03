ac976010aac02f57ad74f231611f56ab03a8f915
d-babel <143659056+d-babel@users.noreply.github.com>
2024-01-03 22:43:22 +0000

    Wed, Jan 3, 2024, 2:43 PM -08:00

---
+++ 2324/chapter3/examples/snowflake/snowflake.c
@@ -0,0 +1,39 @@
+// Snowflake Demo
+#include <stdio.h>
+#define DIM 10
+
+void init(void);
+void draw(void);
+// TODO #1
+// Declare a 2D char array like this:
+// data-type board[size][size]
+
+int main(void)
+{
+    init();
+    draw();
+}
+
+// Fills the array
+void init(void)
+{
+    // TODO #2
+    // fill 2d array with *s
+    // Optionally, change certain locations to ' '
+    for (int i = 0; i < DIM; i++)
+    {
+        // Another nested loop goes here
+    }
+}
+
+void draw(void)
+{
+    // TODO #3
+    // Nested loop
+    // Write the code that prints the board.
+    for (int i = 0; i < DIM; i++)
+    {
+
+    }
+}
+
