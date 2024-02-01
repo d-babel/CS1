cec45b5a21f4ea41e0764df07a1346ea7bb8e65d
d-babel <143659056+d-babel@users.noreply.github.com>
2024-02-01 17:31:26 +0000

    Thu, Feb 1, 2024, 9:31 AM -08:00

--- 2324/chapter4/examples/filter/helpers.c
+++ 2324/chapter4/examples/filter/helpers.c
@@ -1,25 +1,74 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++){
+        for (int j = 0; j < width; j++){
+
+            //calc average via the 255 RGB values and set to avg
+            BYTE avg = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
+
+            //set each color to according avg
+            image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = avg;
+        }
+    }
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++){
+        //width / 2 --> flipping half of the image over the center
+        for (int j = 0; j < width / 2; j++){
+
+            //swap pixels --> is there a function/better way to do this?
+            RGBTRIPLE temp = image[i][j];
+            //width - 1 - j  --> mirroring pixels ACROSS CENTER
+            image[i][j] = image[i][width - 1 - j];
+            image[i][width - 1 - j] = temp;
+        }
+    }
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++){
+        for (int j = 0; j < width; i++){
+
+            //vars for sum of pixels
+            int sumRed, int sumGreen, int sumBlue;
+            int count = 0;
+
+            //interate over neighboring pixels
+            //"di" & "dj" meaning delta i and delta j --> apparently its "good practice"
+            //-1, 0,1 --> accesses entire 3x3 grid, (0,0) being the middle pixel
+            for (int di = -1; di <= 1; di++){
+                for (int dj = -1; dj <= 1; dj++){
+
+                    int newRow = i + di;
+                    int newCol = j + dj;
+
+                    
+                }
+            }
+
+
+        }
+    }
+
+        }
+    }
     return;
 }
 
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+
     return;
 }
