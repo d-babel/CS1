107d9248547b90cac547617aa320826d130643c9
d-babel <143659056+d-babel@users.noreply.github.com>
2024-02-13 21:57:48 +0000

    Tue, Feb 13, 2024, 1:57 PM -08:00

--- 2324/chapter4/examples/filter/helpers.c
+++ 2324/chapter4/examples/filter/helpers.c
@@ -160,4 +160,6 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
 void artwork(int height, int width, RGBTRIPLE image[height][width])
 {
   // TODO: Write code that modifies the image
+
+  
 }
