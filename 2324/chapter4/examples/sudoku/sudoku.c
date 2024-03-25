f82d3cd440bb56d9d4b192551e69cc073a732543
d-babel <143659056+d-babel@users.noreply.github.com>
2024-03-25 20:39:12 +0000

    Mon, Mar 25, 2024, 1:39 PM -07:00

--- 2324/chapter4/examples/sudoku/sudoku.c
+++ 2324/chapter4/examples/sudoku/sudoku.c
@@ -224,6 +224,7 @@ int main(int argc, char *argv[])
             if (g.initialBoardEditable[g.y][g.x]){
                 int moveResult = checkMove(g.y, g.x, ch - '0');
                 switch (moveResult){
+                    hide_banner();
                     case VALID_MOVE:
                     //modify num
                     g.board[g.y][g.x] = ch;
