edeb49e2334f0d16df2f53859248251a38d0c5fc
d-babel <143659056+d-babel@users.noreply.github.com>
2024-03-26 14:49:15 +0000

    Tue, Mar 26, 2024, 7:49 AM -07:00

--- 2324/chapter4/examples/sudoku/sudoku.c
+++ 2324/chapter4/examples/sudoku/sudoku.c
@@ -33,6 +33,7 @@
 #define PAIR_USER_NUM 3
 
 // valid moves
+#define VALID_MOVE 0
 #define BAD_ROW 1
 #define BAD_COL 2
 #define BAD_BOX 3
@@ -278,15 +279,13 @@ bool checkMove(int row, int col, int num)
     for (int i = 0; i < 9; i++)
     {
         if (g.board[row][i] == num && i != col)
-            show_banner("bad row");
-            return true;
+            return BAD_ROW;
     }
     // check col
     for (int i = 0; i < 9; i++)
     {
         if (g.board[i][col] == num && i != row)
-            show_banner("bad col");
-            return true;
+            return BAD_COL;
     }
     // check 3x3 box
     int startRow = row - row % 3, startCol = col - col % 3;
@@ -295,10 +294,11 @@ bool checkMove(int row, int col, int num)
         for (int j = 0; j < 3; j++)
         {
             if (g.board[i + startRow][j + startCol] == num)
-                show_banner("bad box");
-                return true;
+                return BAD_BOX;
         }
     }
+    // no conflict
+    return VALID_MOVE;
 }
 
 // Draw's the game's board
