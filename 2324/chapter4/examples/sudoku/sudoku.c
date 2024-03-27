6d85ff86295b31b2557283d27c12a13f015cb45e
d-babel <143659056+d-babel@users.noreply.github.com>
2024-03-27 05:02:46 +0000

    Tue, Mar 26, 2024, 10:02 PM -07:00

--- 2324/chapter4/examples/sudoku/sudoku.c
+++ 2324/chapter4/examples/sudoku/sudoku.c
@@ -242,6 +242,7 @@ int main(int argc, char *argv[])
                 //         break;
                 //     }
                 // }
+                break;
 
             // deletion
             case '0':
