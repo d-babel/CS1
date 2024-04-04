1188dafcf7603b4bf74ac99dccf1fb33e74fb1e1
d-babel <143659056+d-babel@users.noreply.github.com>
2024-04-04 14:52:09 +0000

    Thu, Apr 4, 2024, 7:52 AM -07:00

--- 2324/chapter5/examples/sentimental/mario/mario.py
+++ 2324/chapter5/examples/sentimental/mario/mario.py
@@ -2,14 +2,13 @@
 
 # recieving user unput
 while True:
-    try:
-        height = input("height: ")
-        # check for non-valid inputs
-        if height.isdigit() and 1 <= int(height) <= 8:
-            height = int(height)
-            break
-        else:
-            print("height must be between 1 and 8")
+    height = input("height: ")
+    # check for non-valid inputs
+    if height.isdigit() and 1 <= int(height) <= 8:
+        height = int(height)
+        break
+    else:
+        print("height must be an int between 1 and 8")
 
 # printing mario pyramid
 for i in range(1, height + 1):
