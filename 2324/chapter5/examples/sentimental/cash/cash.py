bec6d881194149b55a4a9b366e7a7b8e87f27462
d-babel <143659056+d-babel@users.noreply.github.com>
2024-04-04 15:02:35 +0000

    Thu, Apr 4, 2024, 8:02 AM -07:00

--- 2324/chapter5/examples/sentimental/cash/cash.py
+++ 2324/chapter5/examples/sentimental/cash/cash.py
@@ -1,11 +1,4 @@
 # Cash by Drew Babel
 
-def get_positive_float(prompt):
-    while True:
-        user_input = input(prompt)
-        try:
-            value = float(user_input)
-            if value > 0:
-                return value
-            else:
-                
+
+
