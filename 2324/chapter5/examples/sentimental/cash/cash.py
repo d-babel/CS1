903d03cd2f3de5d61891576aaa705e92b452588e
d-babel <143659056+d-babel@users.noreply.github.com>
2024-04-04 15:07:24 +0000

    Thu, Apr 4, 2024, 8:07 AM -07:00

--- 2324/chapter5/examples/sentimental/cash/cash.py
+++ 2324/chapter5/examples/sentimental/cash/cash.py
@@ -5,6 +5,15 @@ while True:
     if cash > 0:
         break
 
-    
+coins = int(cash * 100)
 
+# list of coin values
+coin_values = [25, 10, 5, 1]
+count = 0
 
+for value in coin_values:
+    while coins >= value:
+        coins -= value
+        count += 1
+
+print(count)
