7ae071cf09f8383fa1af51ecf134044f558f79da
d-babel <143659056+d-babel@users.noreply.github.com>
2024-04-04 14:50:33 +0000

    Thu, Apr 4, 2024, 7:50 AM -07:00

--- 2324/chapter5/examples/sentimental/mario/mario.py
+++ 2324/chapter5/examples/sentimental/mario/mario.py
@@ -1,19 +1,19 @@
 # Mario by Drew Babel
 
-#recieving user unput
+# recieving user unput
 while True:
     try:
         height = input("height: ")
-        #check for non-valid inputs
+        # check for non-valid inputs
         if height.isdigit() and 1 <= int(height) <= 8:
             height = int(height)
             break
         else:
             print("height must be between 1 and 8")
 
-#printing mario pyramid
+# printing mario pyramid
 for i in range(1, height + 1):
     spaces = ' ' * (height - i)
     hashes = '#' * i
-    #f-string allows string literals "{}"
+    # f-string allows string literals "{}"
     print(f"{spaces}{hashes}  {hashes}")
