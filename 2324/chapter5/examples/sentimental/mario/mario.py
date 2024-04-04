37ecd02e37a1ae6e16e55adb337b5d28fe703b2a
d-babel <143659056+d-babel@users.noreply.github.com>
2024-04-04 14:42:26 +0000

    Thu, Apr 4, 2024, 7:42 AM -07:00

--- 2324/chapter5/examples/sentimental/mario/mario.py
+++ 2324/chapter5/examples/sentimental/mario/mario.py
@@ -1,3 +1,16 @@
 # Mario by Drew Babel
 
+#recieving user unput
+while True:
+    try:
+        height = int(input("height: "))
+        if 1 <= height <= 8:
+            break
+        else:
+            print("height must be between 1 and 8")
 
+#printing mario pyramid
+for i in range(1, height + 1):
+    spaces = ' ' * (height - i)
+    hashes = '#' * i
+    print(f)
