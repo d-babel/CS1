d3ac4faf1060fb2126d7f267503586ee4a8a88ee
d-babel <143659056+d-babel@users.noreply.github.com>
2024-04-05 17:13:49 +0000

    Fri, Apr 5, 2024, 10:13 AM -07:00

---
+++ 2324/chapter5/examples/salad/salad.py
@@ -0,0 +1,29 @@
+# Salad by Drew Babel
+
+# Let's create a Salad!
+# Read this: https://www.w3schools.com/python/python_ref_list.asp
+
+# Create a salad with five different ingredients.
+# One of them must be a carrot.
+salad = ["carrot", "lettuce", "tomato", "chicken", "ranch"]
+
+# Print the ingredients in your salad.
+print(salad)
+
+# Add three new ingredients to your salad. One of them must be a carrot.
+salad.append("spinach")
+salad.append("cucumbers")
+salad.append("peppers")
+
+print(salad)
+# Print the ingredients in your salad.
+
+# Now, use a Python list method to remove ALL carrots from your salad. #nocarrots
+
+# Create a separate function that removes carrots from a salad.
+
+# Create a function that removes any given ingredient from a salad.
+
+# Create a function that builds a new salad by doing the following:
+#   Prompt the user for the number of ingredients
+#   Prompt the user for each ingredient and edd it to salad
