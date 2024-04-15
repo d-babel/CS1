a23b31a73cd3eb755ed2c1e473f14e11f5cc7156
d-babel <143659056+d-babel@users.noreply.github.com>
2024-04-15 16:21:35 +0000

    Mon, Apr 15, 2024, 9:21 AM -07:00

--- 2324/chapter5/examples/worldcup/tournament.py
+++ 2324/chapter5/examples/worldcup/tournament.py
@@ -59,7 +59,10 @@ def simulate_round(teams):
 
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
-    # TODO
+    while len(teams) > 1:
+        teams = simulate_round(teams)
+    #return last remaining team name
+    return teams[0]['team']
 
 
 if __name__ == "__main__":
