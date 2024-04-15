383e5c53cb2bacbf1b1935695a130b8a8d8aeaa8
d-babel <143659056+d-babel@users.noreply.github.com>
2024-04-15 21:24:25 +0000

    Mon, Apr 15, 2024, 2:24 PM -07:00

---
+++ 2324/chapter5/examples/garage/garage.py
@@ -0,0 +1,24 @@
+# Read about Python dictionaries here: https://www.w3schools.com/python/python_dictionaries.asp
+# car1 is a dict. It contains keys and values. "make" is a key. "Toyota" is a value.
+car1 = {"make":"Toyota", "model":"Corolla", "year":"2015", "color":"green"}
+
+# This returns the value associated with the key "make"
+str = car1["make"]
+print(str)
+
+# print out "I have a green 2015 Toyota Corolla" where the values are filled in from the dict.
+
+# create at least three more cars with keys for make, model, year, and color.
+
+# create a list called "garage" that contains all of these cars.
+
+# print the contents of your garage.
+#print(garage)
+
+# You got a brand new car! You get to choose the make, model, year, and color.
+# Now add it to the garage. Bonus if you can create the car and add it
+# in a single line of code.
+
+# Iterate through the list and print out "I have a [color] [make] [model]" for each car in the garage.
+
+# Iterate through the list and print out every car that is newer than 2020.
