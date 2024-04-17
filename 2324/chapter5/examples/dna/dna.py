1665c4c8516aa4f9c2bad5a8ad9b8063b197044b
d-babel <143659056+d-babel@users.noreply.github.com>
2024-04-17 15:18:41 +0000

    Wed, Apr 17, 2024, 8:18 AM -07:00

--- 2324/chapter5/examples/dna/dna.py
+++ 2324/chapter5/examples/dna/dna.py
@@ -3,23 +3,24 @@
 import csv
 import sys
 
-def main():
-    #check usage
-    if len(sys.argv) != 3:
-        sys.exist("usage: python dna.py database.csv sequence.txt")
 
-    #read database and DNA sequence
+def main():
+    # check usage
+    if len(sys.argv) != 3:
+        sys.exit("usage: python dna.py database.csv sequence.txt")
+
+    # read database and DNA sequence
     database, dna_sequence = read_files(sys.argv[1], sys.argv[2])
 
-    #find largest consecutive repeats for each STR in DNA sequence
+    # find largest consecutive repeats for each STR in DNA sequence
     str_counts = {
         str_key: longest_run(dna_sequence, str_key)
         for str_key in database[0].keys()
         if str_key != "name"
     }
 
-    #compare database STR counts to DNA sequence STR counts
-    for person in databse:
+    # compare database STR counts to DNA sequence STR counts
+    for person in database:
         if match_person(person, str_counts):
             print(person['name'])
             return
@@ -28,25 +29,25 @@ def main():
 
 
 def read_files(database_path, sequence_path):
-    #load database from CSV file
+    # load database from CSV file
     with open(database_path, newline='') as file:
         reader = csv.DictReader(file)
         database = list(reader)
 
-    #load DNA sequence from text file
+    # load DNA sequence from text file
     with open(sequence_path, 'r') as file:
         dna_sequence = file.read().strip()
 
-    return databse, dna_sequence
+    return database, dna_sequence
 
 
 def longest_run(sequence, str_key):
-    #find max num that STR appears consecutively in the sequence
+    # find max num that STR appears consecutively in the sequence
     max_count = 0
     n = len(str_key)
     for i in range(len(sequence)):
         count = 0
-        while sequence[i + n*count: i + n*(count + 1)] == str.key:
+        while sequence[i + n*count: i + n*(count + 1)] == str_key:
             count += 1
         if count > max_count:
             max_count = count
@@ -55,7 +56,7 @@ def longest_run(sequence, str_key):
 
 
 def match_person(person, str_counts):
-    #check if STR couts match in person's STR counts in database
+    # check if STR couts match in person's STR counts in database
     return all(int(person[str_key]) == str_counts[str_key] for str_key in str_counts)
 
 
